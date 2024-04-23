4ea90f9e3663c26d9dca68f29cf60aee72ca1d50
Samyak Surti <spsurti@berkeley.edu>
2024-04-22 21:44:21 -0700

    Working on HCD Oracle

---
+++ hcd_oracle.py
@@ -0,0 +1,12 @@
+from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
+from qiskit.circuit.library import QFT
+from qiskit.circuit.library import ZGate
+# from qiskit.circuit.library import cz
+from qiskit.circuit.library import IntegerComparator
+import numpy as np
+
+def index_forwarder():
+    
+
+def apply_hcd(circuit : QuantumCircuit, registers, constants):
+    pass
